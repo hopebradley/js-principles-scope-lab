b84cb5cec0ee9d4c40aeb745b51f764b24f52841
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-16 16:43:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// PROBLEMATIC FUNCTION //
+
+function setBestCustomer() {
+  var bestCostumer = 'not bob';
+}
+
+// seemingly the end of errors... //
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = 'betty';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'amir';
+}
